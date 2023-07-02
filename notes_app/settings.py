3712824cb5c45b99b25b78739459dd421238ef85
Akinwande Tomisin <tomjames156@gmail.com>
2023-07-02 15:40:57 +0100

    updated allowed hosts

--- notes_app/settings.py
+++ notes_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://fullstacknotesapp-production.up.railway.app/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
