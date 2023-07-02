5ba982186d324ce1e6184c62b42ab06c0e24b413
Akinwande Tomisin <tomjames156@gmail.com>
2023-07-02 15:22:54 +0100

    fixed bug

--- notes_app/urls.py
+++ notes_app/urls.py
@@ -16,13 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
-from django.conf import settings
-from django.conf.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include('api.urls'))
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
