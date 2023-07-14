3c0b3b7fa5cd9c6c989d041c2d180a2c84ae65aa
Akinwande Tomisin <tomjames156@gmail.com>
2023-07-14 15:45:26 +0100

    updated the allowed cors origins

--- notes_app/settings.py
+++ notes_app/settings.py
@@ -139,4 +139,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = 'https://notes-app-react-tan.vercel.app/'
