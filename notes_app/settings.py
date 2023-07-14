9e32dd5b6653e8971eda31703a5932128e2437eb
Akinwande Tomisin <tomjames156@gmail.com>
2023-07-14 15:49:59 +0100

    updated setting.py

--- notes_app/settings.py
+++ notes_app/settings.py
@@ -140,5 +140,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    'https://notes-app-react-tan.vercel.app/'
+    'https://notes-app-react-tan.vercel.app/',
+    'https://fullstacknotesapp-production.up.railway.app'
 ]
